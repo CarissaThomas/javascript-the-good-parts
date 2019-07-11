67dea815ee221eb797dc4f4e74cd083cad4ab48a
Carissa Thomas <cthomas@callruby.com>
2019-07-11 13:36:14 -0700

    chapter 1: statements

---
+++ exercises/chapter1-statements.js
@@ -0,0 +1,15 @@
+//CHAPTER 1: STATEMENT EXERCIES
+
+//1. Write a function that takes a string as a parameter and console logs whether or not the parameter is equal to 'cat'
+
+//2. Write a function that takes a song name and returns the corresponding artist (3 possible cases)
+
+//3. Write a function with a for loop that takes an array of integers as a paramter and console logs every array integer + 1
+
+//4. Write a function with a while loop that breaks once a string reaches a length bigger than 6 
+
+//5. Write a function with a for each loop that takes an array of integers as a parameter.  Add together every element in the array and return the sum. 
+
+//6. Write a function with a for loop that takes an array of strings as a parameter that breaks once a specific string is found
+
+//7. Write a function that throws an error if two string parameters are exactly the same
