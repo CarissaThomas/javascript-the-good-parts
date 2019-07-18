2e8ab60c28667fef79e23b7489c1b8e7e0b25650
Carissa Thomas <cthomas@callruby.com>
2019-07-18 10:52:49 -0700

    chapter 1 expressions-operators

---
+++ exercises/chapter1-expressions-operations.js
@@ -0,0 +1,13 @@
+//1. Write a for loop that multiples every value in an array and returns the total
+
+//2. Write a function that tests if a parameter is even using the remainder (%) operator
+
+//3. Write a function that returns true if the 1st parameter is greater than or equal to the 2nd paramter AND the 1st paramter is not greater than 5
+
+//4. Write a function that returns false if the 1st parameter is less than or equal to the 2nd paramter OR the 1st paramter is greater than 21
+
+//5. Write a function using a ternary operator that checks if a string's length is longer than 5
+
+//6. Write a function with a while loop that runs as long as y is less than 3 (tip: dont let the loop run forever)
+
+//7. Write a function that iterates through an array of integers and checks if each integer has a remainder.  If there is a remainder add it to a total.  Return the total.
