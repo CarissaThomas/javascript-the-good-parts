3e1b8ec79a44f68bf6b64e6b740733502bf7c043
Carissa Thomas <cthomas@callruby.com>
2019-08-15 10:55:44 -0700

    corrections for expressions exercises

--- exercises/chapter1-expressions-operations.js
+++ exercises/chapter1-expressions-operations.js
@@ -24,7 +24,7 @@ function isEven(x) {
 //3. Write a function that returns true if the 1st parameter is greater than or equal to the 2nd paramter AND the 1st paramter is not greater than 5
 
 function usingAndOperator(x,y) {
-    if((x >= y || x === y) && x < 5) {
+    if((x >= y || x === y) && x < 5) {// GREATER THAN OR EQUAL IS EXPRESSED WITH >=  YOU DO NOT NEED THE OTHER || X === Y STATEMENT
         console.log('That is true!'); // you can also just write this as return true
     } else {
         console.log('That is false!');
@@ -60,18 +60,33 @@ function whileLoopsYay() {
 //7. Write a function that iterates through an array of integers and checks if each integer has a remainder.  If there is a remainder add it to a total.  Return the total.
 
 let testArray = [1, 2, 3, 4, 5, 6, 7];
+console.log(remaindersAreHard(testArray));
+// function remaindersAreHard(integerArray) {
+//     let total = 0;
+//     for(i=0; i <= integerArray.length; i++) {
+//         if (i %2 !== 0) {
+//             total = i++;
+//         }
+
+//     }
+
+//     console.log(total)
+// }
 
 function remaindersAreHard(integerArray) {
-    let total = 0;
-    for(i=0; i <= integerArray.length; i++) {
-        if (i %2 !== 0) {
-            total = i++;
-        }
+    var total = 0;
 
+    for(var i = 0; i < integerArray.length; i++) {//be careful, you need to use a < in your for loop not <= 
+        if (integerArray[i] % 2 !== 0) {//integerArray[i] gives us the actual value in the array vs i is just the index number (i.e. 0, 1, 2, 3, 4, 5) no matter the values inside the arrays
+            total += integerArray[i];// use += syntax to add a number to an existing value insted of overwriting it 
+            //total = i++; be careful messing with i will change the order of your loop.  i is the counter in your loop if you do i++ you are manually incrementing it 
+        }
     }
 
-    console.log(total)
+    return total;
 }
+
+
 
 
 // I did the above because I wasn't sure what I was supposed to be checking each integer with. Example: 1 % ???
